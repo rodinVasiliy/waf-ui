4c20cb62f87a80d010aec2917741c3d61cc253e5
varod <varodg2000@mail.ru>
2026-04-11 18:26:08 +0300

    fix api request function

--- src/api/apirequest.ts
+++ src/api/apirequest.ts
@@ -1,17 +1,17 @@
 
 
-export async function apiRequest(url:string, options?: RequestInit) {
-    const res = await fetch(url, options)
-    
-    if (!res.ok){
-        let body
-        try{
-            body = await res.json()
-        } catch {
-            throw new Error("Unknown backend error")
-        }
-        throw new Error(body?.message || "Request failed")
+export async function apiRequest(url: string, options?: RequestInit) {
+  const res = await fetch(url, options)
+
+  if (!res.ok) {
+    try {
+      const body = await res.json()
+      throw body
+    } catch {
+      throw new Error("Unknown backend error")
     }
-    const text = await res.text()
-    return text ? JSON.parse(text) : null
+  }
+
+  const text = await res.text()
+  return text ? JSON.parse(text) : null
 }
