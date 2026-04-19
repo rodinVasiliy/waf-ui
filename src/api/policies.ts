f69946a2deb7fb789e4e275549ec523d9f156355
varod <varodg2000@mail.ru>
2026-04-19 10:49:48 +0300

    Use policy list item instead old policy

--- src/api/policies.ts
+++ src/api/policies.ts
@@ -1,8 +1,8 @@
-import type { Policy } from "../types/Policy";
+import type { PolicyListItem } from "../types/Policy";
 
 const API_URL = "/admin/api/policies"
 
-export async function fetchPolicies(): Promise<Policy[]> {
+export async function fetchPolicies(): Promise<PolicyListItem[]> {
     const res = await fetch(API_URL)
     if (!res.ok) {
         throw Error("Failed to get policies")
